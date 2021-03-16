674924155e0ccc3dbb590c93ed799a7703f9635b
ploboda <ploboda@concisesoftware.com>
2021-03-16 08:22:45 +0100

    data model and image resources added

---
+++ app/src/main/java/com/example/androiddevchallenge/Data.kt
@@ -0,0 +1,30 @@
+package com.example.androiddevchallenge
+
+class Item(val name: String, val image: Int)
+
+val favoriteItemsData = listOf(
+    Item(name = "Short mantras", image = R.drawable.i3560168),
+    Item(name = "Nature Meditations", image = R.drawable.i3571551),
+    Item(name = "Stress and anxiety", image = R.drawable.i1557238),
+    Item(name = "Self-massage", image = R.drawable.i1029604),
+    Item(name = "Overwhelmed", image = R.drawable.i3560044),
+    Item(name = "Nightly wind down", image = R.drawable.i924824)
+)
+
+val bodyItemsData = listOf(
+    Item(name = "Inversions", image = R.drawable.i317157),
+    Item(name = "Quick yoga", image = R.drawable.i1812964),
+    Item(name = "Stretching", image = R.drawable.i4056723),
+    Item(name = "Tabata", image = R.drawable.i4662438),
+    Item(name = "HILT", image = R.drawable.i999309),
+    Item(name = "Pre-natal yoga", image = R.drawable.i396133)
+)
+
+val mindItemsData = listOf(
+    Item(name = "Meditate", image = R.drawable.i3822622),
+    Item(name = "With kids", image = R.drawable.i3094230),
+    Item(name = "Aromatherapy", image = R.drawable.i4498318),
+    Item(name = "On the go", image = R.drawable.i1241348),
+    Item(name = "With pets", image = R.drawable.i4056535),
+    Item(name = "High stress", image = R.drawable.i897817)
+)
